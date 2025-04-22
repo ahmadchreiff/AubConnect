86046f8189135809bf76455598ec266324873f52
Chregar <shreifahmad15@gmail.com>
2025-04-22 11:31:15 +0300

    Fixed footer

--- frontend/src/pages/Components/Footer.jsx
+++ frontend/src/pages/Components/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = () => {
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
             <div className="flex items-center space-x-1 text-[#860033]">
-              <div className="h-8 w-8">
+              {/* <div className="h-8 w-8">
                 <svg viewBox="0 0 100 100" className="h-full w-full fill-current">
                   <path d="M50,15 C35,15 25,25 25,40 C25,50 30,55 40,65 C45,70 50,85 50,85 C50,85 55,70 60,65 C70,55 75,50 75,40 C75,25 65,15 50,15 Z"></path>
                 </svg>
-              </div>
-              <span className="font-serif text-lg">AubConnect</span>
+              </div> */}
+              {/* <span className="font-serif text-lg">AubConnect</span> */}
             </div>
             
             <div className="flex items-center space-x-6">
