3137782b4f1b07c26417eefabe08c474b8e91a90
Chregar <shreifahmad15@gmail.com>
2025-04-22 14:29:40 +0300

    Fixed the final deployment branch

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     // console.log('MongoDB connected'); 
   } catch (err) {
-    // console.error('MongoDB connection error:', err.message);
     throw err; // Throw the error to be handled in server.js
   }
 };
